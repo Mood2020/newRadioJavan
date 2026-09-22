15ead053268b6121eba2c8fcfeb47237d078fe15
Mood <64140860+Mood2020@users.noreply.github.com>
2026-09-22 20:34:28 +0330

    fix(android): correct Compose layout direction import

--- android-app/app/src/main/java/com/mood2020/radiojavan/MainActivity.kt
+++ android-app/app/src/main/java/com/mood2020/radiojavan/MainActivity.kt
@@ -88,12 +88,12 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.LayoutDirection
-import androidx.compose.ui.unit.LocalLayoutDirection
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.core.content.ContextCompat
